d6922e451c45065bf3a6393c6d99a502b9fe3fdc
MoneyXprt <ian@moneyxprt.com>
2025-08-28 05:51:46 -0700

    Delete app/auth/callback/page.tsx

--- app/auth/callback/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/auth/callback/page.tsx
-import CallbackClient from './CallbackClient';
-
-export const dynamic = 'force-dynamic'; // don't prerender this auth route
-
-export default function Page() {
-  return <CallbackClient />;
-}
