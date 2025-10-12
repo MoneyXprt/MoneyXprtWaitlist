790458c439b0a651d40a4a62c236ccb738906b93
MoneyXprt <ian@moneyxprt.com>
2025-10-12 07:20:50 -0700

    test(ai): add smoke check for Explain My Score flow (if e2e present)

--- types/shims.d.ts
+++ types/shims.d.ts
@@ -2,6 +2,8 @@ declare module 'vitest' {
   export const describe: any
   export const it: any
   export const expect: any
+  export const beforeEach: any
+  export const afterEach: any
 }
 declare module 'vitest/config' {
   export const defineConfig: any
@@ -11,14 +13,23 @@ declare module 'pdf-lib' {
   export const StandardFonts: any
 }
 declare module 'stripe' {
-  const Stripe: any
-  export default Stripe
+  export default class Stripe {
+    constructor(apiKey: string, config?: any)
+    webhooks: { constructEvent: (raw: string, sig: string, secret: string) => any }
+    checkout: { sessions: { create: (args: any) => Promise<any> } }
+  }
+}
+declare namespace Stripe {
+  type Event = any
+  namespace Checkout { type Session = any }
 }
 declare module 'zustand' {
   export type StateCreator<T> = (set: any, get: any) => T
-  export function create<T>(init: StateCreator<T>): {
+  export interface UseBoundStore<T> {
     (): T
     <U>(selector: (state: T) => U): U
+    getState: () => T
+    setState: (partial: Partial<T> | ((state: T) => Partial<T>)) => void
   }
+  export function create<T>(init: StateCreator<T>): UseBoundStore<T>
 }
-
